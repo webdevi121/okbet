09a0805adeed85fc6e3d8b78ec4fc0f468a20796
Sarniel Morgia <sarniel@Sarniels-MBP.gateway>
2023-04-06 12:35:53 +1000

    share functionality

--- src/templates/details.js
+++ src/templates/details.js
@@ -2,9 +2,18 @@ import React from "react"
 import { graphql } from "gatsby"
 import Seo from "components/seo"
 import Layout from "components/layout"
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  FacebookIcon,
+  FacebookMessengerIcon,
+  TwitterIcon,
+  FacebookMessengerShareButton,
+} from "react-share"
 
 const DetailPage = ({ data }) => {
   const item = data.wpPost
+  const postUrl = data.site.siteMetadata.siteUrl + item.link
 
   return (
     <React.Fragment>
@@ -12,11 +21,25 @@ const DetailPage = ({ data }) => {
         <Seo
           title={item.acfSeoData.seoTitle}
           description={item.acfSeoData.seoDescription}
+          image={item.acfSeoData.socialThumbnail?.sourceUrl}
+          uri={item.link}
         />
         <div className="sir-container">
           <div className="flex space-x-3">
             <div className="space-y-5 text-left">
               <h1 className="text-primary text-3xl font-bold">{item.title}</h1>
+              <div>
+                <h2 className="text-3xl font-bold">Share:</h2>
+                <FacebookShareButton url={postUrl}>
+                  <FacebookIcon size={32} round={true} />
+                </FacebookShareButton>
+                <FacebookMessengerShareButton url={postUrl}>
+                  <FacebookMessengerIcon size={32} round={true} />
+                </FacebookMessengerShareButton>
+                <TwitterShareButton url={postUrl}>
+                  <TwitterIcon size={32} round={true} />
+                </TwitterShareButton>
+              </div>
               <div dangerouslySetInnerHTML={{ __html: item.content }} />
             </div>
           </div>
@@ -39,6 +62,9 @@ export const query = graphql`
       acfSeoData {
         seoDescription
         seoTitle
+        socialThumbnail {
+          sourceUrl
+        }
       }
     }
     allWpCategory(filter: { count: { gt: 0 } }) {
@@ -49,6 +75,11 @@ export const query = graphql`
           slug
           id
         }
+      }
+    }
+    site {
+      siteMetadata {
+        siteUrl
       }
     }
   }
