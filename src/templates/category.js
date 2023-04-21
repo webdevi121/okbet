4c033e65b5b14f4058313d49bf0fcae997b28911
Sarniel Morgia <sarniel@Sarniels-MBP.gateway>
2023-04-21 14:24:49 +1000

    updates

--- src/templates/category.js
+++ src/templates/category.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 import { graphql } from "gatsby"
 import Seo from "components/seo"
 import Layout from "components/layout"
 import SubCategoryMenu from "../components/subCategoryMenu"
 import FeaturedPost from "../components/featuredPost"
+import { ChevronRightIcon } from "@heroicons/react/solid"
 
 const CategoryPage = ({ data }) => {
   const category = data.wpCategory
@@ -15,16 +16,33 @@ const CategoryPage = ({ data }) => {
     <React.Fragment>
       <Layout>
         <Seo title="title" description="description" />
-        <div className="sir-container">
-          <div className="mb-10">
-            <h1 className="text-3xl font-bold">Latest: {category.name}</h1>
-          </div>
-          <div className="flex space-x-10">
-            <SubCategoryMenu data={category} />
-            <div className="w-full space-y-3">
-              <div className="category-layout grid grid-cols-3 gap-5">
-                <FeaturedPost data={list} />
-              </div>
+        <div className="mb-5 w-full">
+          <ul className="flex">
+            <li className="flex items-center">
+              <a href="" className="p-2">
+                Home
+              </a>
+            </li>
+            <li className="flex items-center">
+              <ChevronRightIcon className="mx-2 w-5" />
+              <a href="" className="p-2">
+                Racing
+              </a>
+            </li>
+            <li className="flex items-center last:opacity-50">
+              <ChevronRightIcon className="mx-2 w-5" />
+              {category.name}
+            </li>
+          </ul>
+        </div>
+        <div className="mb-10">
+          <h1 className="text-3xl font-bold">Latest: {category.name}</h1>
+        </div>
+        <div className="flex space-x-10">
+          <SubCategoryMenu data={category} />
+          <div className="w-full space-y-3">
+            <div className="category-layout grid grid-cols-3 gap-5">
+              <FeaturedPost data={list} />
             </div>
           </div>
         </div>
